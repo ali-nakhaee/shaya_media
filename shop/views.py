66c634339bd39306f739d9b276ac4fcead14e7da
ali-nakhaee <ali.nakhaee.kooshe@gmail.com>
2024-07-21 23:04:56 +0330

    invalid_form method added to Pricing view.

--- shop/views.py
+++ shop/views.py
@@ -16,6 +16,12 @@ class Pricing(View):
         'forms': forms,
         'prices': prices,
         }
+    
+    def invalid_form(self, request, form_name: str, form):
+        """ The specific form will be populated with the data previously submitted. """
+        context = self.context
+        context['forms'][form_name] = form
+        return render(request, "shop/pricing.html", context)
 
     def get(self, request):
         return render(request, "shop/pricing.html", self.context)
@@ -25,23 +31,27 @@ class Pricing(View):
             form = TypeForm(request.POST)
             if form.is_valid():
                 form.save()
-            return render(request, "shop/pricing.html", self.context)
-        
+                return render(request, "shop/pricing.html", self.context)
+            return self.invalid_form(request, 'type_form', form)
+            
         elif 'level_form' in request.POST:
             form = LevelForm(request.POST)
             if form.is_valid():
                 form.save()
-            return render(request, "shop/pricing.html", self.context)
+                return render(request, "shop/pricing.html", self.context)
+            return self.invalid_form(request, 'level_form', form)
         
         elif 'subject_form' in request.POST:
             form = SubjectForm(request.POST)
             if form.is_valid():
                 form.save()
-            return render(request, "shop/pricing.html", self.context)
+                return render(request, "shop/pricing.html", self.context)
+            return self.invalid_form(request, 'subject_form', form)
 
         elif 'price_form' in request.POST:
             form = PriceForm(request.POST)
             if form.is_valid():
                 form.save()
-            return render(request, "shop/pricing.html", self.context)
+                return render(request, "shop/pricing.html", self.context)
+            return self.invalid_form(request, 'price_form', form)
         
