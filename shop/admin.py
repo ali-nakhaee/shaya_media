1e3f1d19d4bbdcf2e9795329235146e7ff2a7fb5
ali-nakhaee <ali.nakhaee.kooshe@gmail.com>
2024-07-23 09:52:16 +0330

    shop.models added to admin panel.

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from .models import Price
+from .models import Price, Type, Subject, Level
 
 @admin.register(Price)
 class PriceAdmin(admin.ModelAdmin):
     list_display = ['id', 'subject', 'level', 'price']
     list_display_links = ['id', 'subject', 'level', 'price']
 
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'unit']
+    list_display_links = ['id', 'type', 'unit']
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'subject']
+    list_display_links = ['id', 'type', 'subject']
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'level_num', 'title']
+    list_display_links = ['id', 'level_num', 'title']
