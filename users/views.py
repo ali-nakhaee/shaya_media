7da49570aa2dc5921fb4773db100b283a7c3d321
ali-nakhaee <ali.nakhaee.kooshe@gmail.com>
2024-08-26 14:24:07 +0330

    CheckPassword view post method completed.

--- users/views.py
+++ users/views.py
@@ -6,6 +6,8 @@ from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout, get_user_model
 from django.contrib import messages
+from django.utils import timezone
+from django.http import Http404
 
 from . import forms
 
@@ -29,10 +31,16 @@ class LoginPage(View):
             except User.DoesNotExist:
                 user = User.objects.create(phone_number=phone_number)
             user.temporary_password = random.randint(1000, 9999)
+            user.password_generation_time = timezone.now()
             user.save()
             messages.success(request, f"رمز موقت شما: {user.temporary_password}")
             request.session['phone_number'] = phone_number
             return redirect('users:check_password')
+        else:
+            context = {'form': form,
+                       'check_password': False,
+                       }
+            return render(request, 'users/login.html', context)
         
 class CheckPassword(View):
     def get(self, request):
@@ -45,18 +53,24 @@ class CheckPassword(View):
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
+        print(f"form.errors: {form.errors}")
         if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['phone_number'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                print(request.POST.get('next'))
-                return redirect(self.request.POST.get('next'))
+            try:
+                user = User.objects.get(phone_number=form.cleaned_data['phone_number'])
+            except User.DoesNotExist:
+                raise Http404
+            if user.temporary_password == form.cleaned_data['password']:
+                login(request, user, backend='users.backends.PhoneNumberAuthBackend')
+                if request.POST.get('next'):
+                    print(request.POST.get('next'))
+                    return redirect(self.request.POST.get('next'))
+                else:
+                    return redirect('shop:orders')
             else:
-                messages.error(request, 'ورود ناموفق. رمز عبور یا نام کاربری اشتباه است.')
-                return render(request, 'users/login.html', {'form':form})
+                messages.error(request, 'ورود ناموفق. رمز عبور اشتباه است.')
+                return redirect('users:login')
+        else:
+            return redirect('users:login')
             
 class Logout(View):
     def get(self, request):
