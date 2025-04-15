b923990c4d55a22b76501b27cf12c27c43e6a2b2
ali-nakhaee <ali.nakhaee.kooshe@gmail.com>
2025-04-15 13:59:03 +0330

    The max_length for password field in users.user model changed to 200 to solve the error in dumpdata to postgreSQL.

--- users/models.py
+++ users/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
         },
     )
 
-    password = models.CharField(max_length=20, null=True, blank=True)
+    password = models.CharField(max_length=200, null=True, blank=True)
 
     phone_number = models.CharField(max_length=30, unique=True,
                                     error_messages={"unique":"اکانت دیگری با این شماره وجود دارد.",
